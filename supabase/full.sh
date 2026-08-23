1f41b8b0b856e886eac3f4710aee5c306d8416e6
SHAN KENNETH BAYON-ON <shankenneth21@gmail.com>
2026-08-24 00:51:07 +0800

    MERGE PREP

--- supabase/full.sh
+++
@@ -1,4 +0,0 @@
-curl.exe -i -X POST "https://ghjfqddhqciycsmxstfs.supabase.co/functions/v1/trigger-scraper" `
-  -H "Authorization: Bearer SUPABASE_ANON_KEY" `
-  -H "Content-Type: application/json" `
-  -d "{\`"mode\`": \`"full\`"}"
